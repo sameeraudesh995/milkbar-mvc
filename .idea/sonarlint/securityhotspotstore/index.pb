7bc5e5523fc47fd7f492c7cd29d4dbbb54e8d488
sameera udesh <73770245+sameeraudesh995@users.noreply.github.com>
2025-01-15 19:02:03 +0530

    milk bar loginform created

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+b
+2src/itp/com/milkbar/view/stylesheets/loginForm.css,a\b\abeacc128d3afdd97eab3c240e5d81121ef50c9f
+g
+7src/itp/com/milkbar/controller/LoginFormController.java,4\e\4e300e0094b0432944cff693d149df8f196ebfef
+W
+'src/itp/com/milkbar/view/LofinForm.fxml,7\f\7f82fe85041a7afcf96e2aaedcf53ca6f5d51a98
---
+++ src/itp/com/milkbar/controller/LoginFormController.java
@@ -0,0 +1,21 @@
+package itp.com.milkbar.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginFormController {
+    public TextField loginUserName;
+    public TextField LoginPassword;
+    public AnchorPane context;
+
+    public void loginOnAction(ActionEvent actionEvent) {
+    }
+
+    public void signUpOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void frogotPasswordOnAction(MouseEvent mouseEvent) {
+    }
+}
